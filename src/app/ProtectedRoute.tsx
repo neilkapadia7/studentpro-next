52ab6c43b53c62b35c213b1fb0fbf86396dc4d65
neilkapadia7 <neilkapadia7@gmail.com>
2024-04-01 00:08:06 +0530

    Todos and redirection added

--- src/app/ProtectedRoute.tsx
+++ src/app/ProtectedRoute.tsx
@@ -1,20 +1,39 @@
 "use client";
-import React, { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { useRouter, usePathname } from 'next/navigation';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/store/store';
 
 const ProtectedRoute = ({children}: Readonly<{children: React.ReactNode}>) => {
+  const [loading, setLoading] = useState(true);
   const router = useRouter();
+  const pathname = usePathname();
   const auth = useSelector((state: RootState) => state.auth);
+  const protectedRoutes = ["/admin", "/dashboard", "/users"];
+
+  function substringMatchesArray(str: string, arr : Array<string>) {
+    for (let i = 0; i < arr.length; i++) {
+        if (str.includes(arr[i])) {
+            return true;
+        }
+    }
+    return false;
+  }
+
+  // Set Global Loading
 
   useEffect(() => {
-    if ((!localStorage.getItem('token') && !auth.loggedIn) || !localStorage.getItem('token')) {
-      router.push('/login');
+    if (
+      substringMatchesArray(pathname, protectedRoutes) &&
+      ((!localStorage.getItem('token') && !auth.loggedIn) || !localStorage.getItem('token'))) {
+      router.push('/');
     }
+    setLoading(false);
   }, []);
 
-  return <>{children}</>;
+    return (
+      <>{children}</>
+    );
 };
 
 export default ProtectedRoute;
